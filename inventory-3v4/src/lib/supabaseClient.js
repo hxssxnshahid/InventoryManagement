6df5b4fa8bfee13c5bde58959d6afb229f309816
hxssxnshahid <hassanshahid2002@outlook.com>
2025-04-14 11:14:37 -0500

    Updated the inventory management frontend

--- inventory-3v4/src/lib/supabaseClient.js
+++ inventory-3v4/src/lib/supabaseClient.js
@@ -1,5 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://vslwxxfeusclldpcogwq.supabase.co'
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZzbHd4eGZldXNjbGxkcGNvZ3dxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDM3Mzk4OTksImV4cCI6MjA1OTMxNTg5OX0.9_Lyt43gP4-iVOUPX9PTc26PJ02TZ7azOdCS0elkEmo'
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
+const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY
+
 export const supabase = createClient(supabaseUrl, supabaseKey) 
